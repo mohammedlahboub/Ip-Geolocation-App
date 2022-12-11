d6733b81c537bd9fa795367dc64582e08e4ca9d4
mohammedlahboub <104053591+mohammedlahboub@users.noreply.github.com>
2022-12-11 22:23:51 +0100

    first commit

---
+++ public/main.js
@@ -0,0 +1,17 @@
+// ! Html elements
+const ipInput = document.querySelector('#inputIp');
+const ipField = document.querySelector('#ipaddress');
+const locationField = document.querySelector('#locattion');
+const timeZoneField = document.querySelector('#timezone');
+const ispField = document.querySelector('#isp');
+
+
+
+// function initialLoad(){
+//     fetch('https://geo.ipify.org/api/v2/country,city?apiKey=at_rnp6P9iqW4qnv5osnohkrAa9CbJA6&ipAddress')
+//      .then(res => res.json())
+//         .then(data => {console.log(data);})
+// }
+
+
+// initialLoad()
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "very-dark-gray":"hsl(0, 0%, 17%)",
+        "dark-gray":"hsl(0, 0%, 59%)"
+      },
+      fontFamily: {
+        rubik: ['Rubik', 'sans-serif'],
+        
+      },
+    },
+    screens: {
+      'sm': '375px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1200px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+  },
+  plugins: [],
+}
